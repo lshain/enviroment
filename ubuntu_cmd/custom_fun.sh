9846bca2405379c607798c0bbf48c96ac98e55a3
lshain <lshain.gyh@gmail.com>
2013-07-04 21:02:42 +0800

    add ubuntu_cmd

---
+++ ubuntu_cmd/custom_fun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function touchpad_off( )
+{
+	synclient touchpadoff=1
+}
+
+function touchpad_on( )
+{
+	synclient touchpadoff=0
+}
+
+function ScrollLock_on( )
+{
+	xset led named 'Scroll Lock'
+}
+
+function ScrollLock_off( )
+{
+	xset -led named 'Scroll Lock'
+}
+
