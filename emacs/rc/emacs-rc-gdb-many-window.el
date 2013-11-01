1728f25abbc29d64244a9b39d6ca77d0700c8a97
lshain <lshain.gyh@gmail.com>
2013-11-01 10:32:24 +0800

    update

--- emacs/rc/emacs-rc-gdb-many-window.el
+++ emacs/rc/emacs-rc-gdb-many-window.el
@@ -1,6 +1,7 @@
 ;;gdb-many-window
 ;;这个功能插件可以使emacs的调试界面像VC一样，有watch, stacktrace等窗口，真正实现图形化gdb.
 ;;在emacs中编译好程序，然后M-x gdb，连按两次ret，多窗口gdb就出来了
+;;gdb --annotate=3 filename
 ;;http://www.inet.net.nz/~nickrob/multi-gud.el
 ;;http://www.inet.net.nz/~nickrob/multi-gdb-ui.el
 
