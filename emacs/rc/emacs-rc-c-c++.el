12235f69b3b731fad8db4ba4022ead3bd48c04f6
lshain <lshain.gyh@gmail.com>
2014-05-27 10:00:14 +0800

    update emacs-rc-c-c++.el

--- emacs/rc/emacs-rc-c-c++.el
+++ emacs/rc/emacs-rc-c-c++.el
@@ -20,6 +20,74 @@
 (c-set-offset 'friend '-)
 (c-set-offset 'substatement-open 0)
 
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+(add-hook 'c-mode-common-hook
+	  '(lambda()
+	     ;;preprocessor
+	     (setq c-macro-prompt-flag t)
+	     (setq c-basic-offset 4)
+	     (setq tab-width 4 indent-tabs-mode t)
+	     (setq fill-column 80)
+	     (setq comment-column 30)
+	     (highlight-parentheses-mode 1)
+	     (tab-indents-region)
+             ;;bind newline-and-indent to RET
+	     (local-set-key (kbd "RET") 'newline-and-indent)
+	     (local-set-key (kbd "C-c ;") 'comment-line)
+	     (local-set-key (kbd "<C-tab>") 'tempo-forward-mark)
+	     (c-toggle-hungry-state 1)
+	     (delete-selection-mode t)
+	     (setq show-trailing-whitespace t) ; can use M-x delete-trailing-whitespace <RET> to delete all trailing whitespace within cur buf
+	     ;cscope configuration
+	     (require 'xcscope)
+	     (setq cscope-do-not-update-database t)
+	     (gtags-mode 1)
+	     (paren-toggle-open-paren-context 1)
+	     (require 'ifdef)
+	     (local-set-key (kbd "C-c I") 'ifdef-mark)
+	     )
+	  )
+
+;; linux kernel codeing style
+(defun c-lineup-arglist-tabs-only (ignored)
+ "Line up argument lists by tabs, not spaces"
+ (let* ((anchor (c-langelem-pos c-syntactic-element))
+ (column (c-langelem-2nd-pos c-syntactic-element))
+ (offset (- (1+ column) anchor))
+ (steps (floor offset c-basic-offset)))
+ (* (max steps 1)
+ c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              ;; Enable kernel mode for the appropriate files
+              (when (and filename
+             (string-match "linux" filename))
+                (setq indent-tabs-mode t)
+                (c-set-style "linux-tabs-only")))))
+
+(defun progmodes-hooks ()
+  "Hooks for programming modes"
+  (add-hook 'before-save-hook 'progmodes-write-hooks))
+
+(defun progmodes-write-hooks ()
+  "Hooks which run on file write for programming modes"
+  (prog1 nil
+    (delete-trailing-whitespace)))
+
+(add-hook 'c-mode-common-hook 'progmodes-hooks)
+
 ;; Setting Tab to indent region if anything i selected
 (defun tab-indents-region ()
   (local-set-key (kbd "<tab>") 'fledermaus-maybe-tab)
